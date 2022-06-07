d5ef934074312de418652e836068aa73197bb632
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2022-06-07 19:58:29 +0200

    Add amule-web package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1950,6 +1950,11 @@ with pkgs;
     client = true;
   };
 
+  amule-web = amule.override {
+    monolithic = false;
+    httpServer = true;
+  };
+
   antennas = nodePackages.antennas;
 
   apg = callPackage ../tools/security/apg { };
